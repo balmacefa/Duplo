bf476769f37a61674df74189fcb79963e0c1a922
Fabian Balmaceda <fabianbalmaceda2@gmail.com>
2016-03-20 10:53:18 -0600

    init class Proceso, estructura

---
+++ Proceso.cpp
@@ -0,0 +1,25 @@
+#include "Proceso.h"
+
+Proceso::Proceso(){
+
+}
+
+void Proceso::hacerAjusteEngrape(){
+
+}
+
+void Proceso::hacerEngrapar(){
+
+}
+
+void Proceso::hacerAvanceDobladora(){
+
+}
+
+void Proceso::hacerDoblar(){
+
+}
+
+boolean Proceso::esTerminado(){
+
+}
---
+++ Proceso.h
@@ -0,0 +1,11 @@
+#include "Arduino.h"
+
+class Proceso{
+  public:
+    Proceso();
+    void hacerAjusteEngrape();
+    void hacerEngrapar();
+    void hacerAvanceDobladora();
+    void hacerDoblar();
+    boolean esTerminado();
+};
