0dd4138219b335956229403933354f8ae0dcc677
Fabian Balmaceda <fabianbalmaceda2@gmail.com>
2016-03-21 22:29:37 -0600

    add Tiempo Test para liberar los ajustes del papel

--- Configuracion.h
+++ Configuracion.h
@@ -6,6 +6,8 @@
 #define TIM_ENTRADA_AJUSTE_HORI_VERT_PAPEL -1//Tiempo donde entran los ajsute del papel para el engrapado
 #define MITAD_VUELTAS_AJUSTE_HORI_VERT_PAPEL 3//Numero de mitad de vueltas
 
+#define TIM_TEST_LIBERAR_AJUSTES -1 //Tiempo para liberar los ajustes para engrapar, el tope y los ajsute de papel
+
 #define TIM_ENTRADA_AJUSTE_HORI_VERT_PAPEL_DEJAR_PASAR -1 //Tiempo dedpues de engrapar para relajar los ajuste de papel y dejar pasar el papel
 #define MITAD_VUELTAS_AJUSTE_HORI_VERT_PAPEL_DEJAR_PASAR 1//Numero de mitad de vueltas
 
