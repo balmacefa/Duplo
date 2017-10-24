900835db486bfdd525310a3f02b076fd1716dfaf
Fabián Balmaceda Rescia <fabianbalmaceda2@gmail.com>
2017-10-24 11:51:00 -0600

    tiempos preliminares

--- Configuracion.h
+++ Configuracion.h
@@ -3,39 +3,38 @@
 
 //Tiempos
 //Engrapadora
-#define TIM_ENTRADA_AJUSTE_HORI_VERT_PAPEL -1//Tiempo donde entran los ajsute del papel para el engrapado
+#define TIM_ENTRADA_AJUSTE_HORI_VERT_PAPEL 1000//Tiempo donde entran los ajsute del papel para el engrapado
 #define MITAD_VUELTAS_AJUSTE_HORI_VERT_PAPEL 3//Numero de mitad de vueltas
 
-#define TIM_TEST_LIBERAR_AJUSTES -1 //Tiempo para liberar los ajustes para engrapar, el tope y los ajsute de papel
 
 #define TIM_ENTRADA_AJUSTE_HORI_VERT_PAPEL_DEJAR_PASAR -1 //Tiempo dedpues de engrapar para relajar los ajuste de papel y dejar pasar el papel
 #define MITAD_VUELTAS_AJUSTE_HORI_VERT_PAPEL_DEJAR_PASAR 1//Numero de mitad de vueltas
 
-#define FRECUENCIA_LECTURA_AJUSTE_PAPEL -1
+#define FRECUENCIA_LECTURA_AJUSTE_PAPEL 64
 
-#define TIM_ENTRADA_ENGRAPAR -1//Tiempo donde se activa el mecanismo para engrapar
-#define TIM_SIG_ENGRAPAR -1//Tiempo que permanece la segnal de engrapado en el rele
-
-
-#define TIM_ENTRADA_TOPE_ENGRAPAR -1//Tiempo donde se activa el tope para el engrape del papel, puede ser el mismo que TIM_ENTRADA_AJUSTE_HORI_VERT_PAPEL
-#define TIM_SIG_TOPE_ENGRAPAR -1//Tiempo que permanece la segnal del tope engrapado en el rele
+#define TIM_ENTRADA_ENGRAPAR 1000//Tiempo donde se activa el mecanismo para engrapar
+#define TIM_SIG_ENGRAPAR 1000//Tiempo que permanece la segnal de engrapado en el rele
 
 
-#define TIM_ENTRADA_AVANCE_PAPEL_ENGRAPADORA -1//Tiempo donde se activa el mecanismo avanzar el papel despues de ser engrapado
-#define TIM_SIG_AVANCE_PAPEL_ENGRAPADORA -1//Tiempo que permanece la segnal de avance del papel hacia la dobladora
+#define TIM_ENTRADA_TOPE_ENGRAPAR 600//Tiempo donde se activa el tope para el engrape del papel, puede ser el mismo que TIM_ENTRADA_AJUSTE_HORI_VERT_PAPEL
+#define TIM_SIG_TOPE_ENGRAPAR 1000//Tiempo que permanece la segnal del tope engrapado en el rele
+
+
+#define TIM_ENTRADA_AVANCE_PAPEL_ENGRAPADORA 1200//Tiempo donde se activa el mecanismo avanzar el papel despues de ser engrapado
+#define TIM_SIG_AVANCE_PAPEL_ENGRAPADORA 500//Tiempo que permanece la segnal de avance del papel hacia la dobladora
 
 //Dobladora
-#define TIM_ENTRADA_TOPE_DOBLADORA -1//Tiempo donde se activa el tope para DOBLAR el papel
-#define TIM_SIG_TOPE_DOBLADORA -1//Tiempo que permanece la segnal del tope DOBLADORA en el rele
+#define TIM_ENTRADA_TOPE_DOBLADORA 1500//Tiempo donde se activa el tope para DOBLAR el papel
+#define TIM_SIG_TOPE_DOBLADORA 500//Tiempo que permanece la segnal del tope DOBLADORA en el rele
 
 
-#define TIM_ENTRADA_DOBLADORA -1//Tiempo donde se activa la dobladora
-#define TIM_SIG_DOBLADORA -1//Tiempo que permanece la segnal de la dobladora en el rele
+#define TIM_ENTRADA_DOBLADORA 1300//Tiempo donde se activa la dobladora
+#define TIM_SIG_DOBLADORA 500//Tiempo que permanece la segnal de la dobladora en el rele
 
-#define  TIM_ENTRADA_BANDAS_DOBLADORA -1 //Tiempo donde se activan las bandas de la dobladora
+#define  TIM_ENTRADA_BANDAS_DOBLADORA 1000 //Tiempo donde se activan las bandas de la dobladora
 
-#define TIM_ENTRADA_SALIDA_CORRIENTE_DOBLADORA -1//Tiempo donde se activa la salida de corriente para la salida del papel o la cortadora
-#define TIM_SIG_SALIDA_CORRIENTE_DOBLADORA  -1//Tiempo que permanece la segnal de la salida de corriente en el rele
+#define TIM_ENTRADA_SALIDA_CORRIENTE_DOBLADORA 1400//Tiempo donde se activa la salida de corriente para la salida del papel o la cortadora
+#define TIM_SIG_SALIDA_CORRIENTE_DOBLADORA  500//Tiempo que permanece la segnal de la salida de corriente en el rele
 
 
 #endif
